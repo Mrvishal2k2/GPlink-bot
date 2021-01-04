767cbec0cdeb2060632d0bbf9f02715c8e877d51
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-01-04 18:13:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
+# Credits to mahesh malekar for his gplinks bot
+# © Mrvishal
 from os import environ
 import aiohttp
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+API_KEY = environ.get('API_KEY')
 
-bot = Client('gplink bot',
+bot = Client('golinksrt bot',
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
@@ -19,7 +22,7 @@ bot = Client('gplink bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm GPlink bot. Just send me link and get short link")
+        "I'm Golinksrt bot. Just send me link and get short link")
 
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
@@ -27,13 +30,15 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [short link]({short_link})', quote=True)
+        button = [[InlineKeyboardButton("Link 🔗", url=short_link)]]
+        markup = InlineKeyboardMarkup(button)
+        await message.reply(f'Here is your shortlink \n`{short_link}`', reply_markup=markup, quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
 
 async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
+    url = 'https://golinksrt.xyz/api'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
